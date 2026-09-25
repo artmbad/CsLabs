3b11eba30edd9e75723937c23d3318809c598bc4
artmbad <1artmbad1@gmail.com>
2026-09-25 20:51:24 +0500

    Initial commit

---
+++ sem-1-lab-00-server-config/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿class program
+{
+
+    public static void Main()
+    {
+        Console.Write("введите имя своего персонажа:");
+        string name = Console.ReadLine();
+        Console.WriteLine($"приветствуем тебя, {name}!!!");
+        Console.Write("какой класс ты выбираешь?");
+        string characterclass = Console.ReadLine();
+        Console.WriteLine("теперь прокачай 3 своих стата");
+        Console.Write("сила:");
+        string buffer1 = Console.ReadLine();
+        int strenght = Convert.ToInt32(buffer1);
+
+        Console.Write("ловкость:");
+        string buffer2 = Console.ReadLine();
+        int agility = Convert.ToInt32(buffer2);
+        
+        Console.Write("интелект:");
+        string buffer3= Console.ReadLine();
+        int intelligence= Convert.ToInt32(buffer3);
+
+        Console.WriteLine($"поздравляем с созданием персонажа, твоё имя:{name}");
+        Console.WriteLine("=============================================");
+        Console.WriteLine($"сила:{strenght}, ловкость:{agility}, интелект:{intelligence}");
+    }
+}
---
+++ sem-1-lab-01-server-config/laba2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace laba2;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("============================================");
+        Console.WriteLine("Cостояние сервера fucking server dota2:");
+        Console.WriteLine("============================================");
+        int numPlayers = 35153;
+        int maxplayerstoday = 42867;
+        double hightping = 1100.31;
+        double lowping = 2.712;
+        float srping = 50.14f;
+        bool serverwork = true;
+        string versiongame = "7.41e";
+
+        Console.WriteLine("-------------Status-------------");
+        Console.WriteLine($"кол-во человек на сервере:{numPlayers}");
+        Console.WriteLine($"максимальный онлайн за сегодня:{maxplayerstoday}");
+        Console.WriteLine($"самый высокий пинг игрока:{hightping}");
+        Console.WriteLine($"самый низкий пинг игрока:{lowping}");
+        Console.WriteLine($"средний пинг игроков{srping}");
+        Console.WriteLine($"сервер работает?{serverwork}");
+        Console.WriteLine($"версия игры:{versiongame}");
+    }
+
+}
